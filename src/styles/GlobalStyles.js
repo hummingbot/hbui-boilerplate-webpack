9812e8769dca92150edaeddf74d31c033475713b
Antonio Brandao <antoniobrandaodesign@gmail.com>
2022-02-15 22:50:26 +0000

    Integrate Satoshi and SF Mono fonts

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Global } from '@emotion/react'
 import { GlobalStyles as BaseStyles } from 'twin.macro'
 import darkModeConfig from '../hb_ui/system/darkModeConfig'
+import './fonts/satoshi/satoshi.css'
+import './fonts/sfmono/sfmono.css'
 
 const GlobalStyles = () => (
   <>
